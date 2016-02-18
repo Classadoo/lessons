b1b505612ac2d3fb94dc257bbb6f25ed08890129
David Gaynor <david.gaynor@students.olin.edu>
2016-02-17 17:22:03 -0800

    generated sample lessons

---
+++ build.sh
@@ -0,0 +1,2 @@
+webpack -p --config lib/webpack.config.js --output-path lib/prod
+webpack -d --config lib/webpack.config.js --output-path lib/dev
---
+++ lessons/Lesson_2.js
@@ -0,0 +1,7 @@
+require("../tasks/SampleTask1");
+require("../tasks/SampleTask2");
+
+__importedLesson = [
+	SampleTask1,
+	SampleTask2
+]
