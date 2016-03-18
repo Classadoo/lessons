75e4fad796c121e6dcad5ba132506360e5f1c26f
David Gaynor <david.gaynor@students.olin.edu>
2016-03-18 10:33:52 -0700

    removed the lego

--- tasks/SampleTask1.js
+++ tasks/SampleTask1.js
@@ -1,6 +1,6 @@
 SampleTask1 = new function() {
 	this.url = "file:///Users/dgaynor/classadoo.github.io/index.html",
-	this.description = "Move the banner from lego starting location",
+	this.description = "Move the banner from it's starting location",
 	this.hint = 'The banner is the big, black bar in the center of the page. Move it by right clicking, then using "inspect element" to change it\'s style'
 	this.hintAction = function() {
 		Helpers.flash($(".intro-container"));
