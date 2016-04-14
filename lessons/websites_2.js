1e5ecb77f7beb875697ae56e48469fe24287a19e
David Gaynor <david.gaynor@students.olin.edu>
2016-04-14 13:49:09 -0700

    added website 2

---
+++ lessons/websites_2.js
@@ -0,0 +1,18 @@
+require("../tasks/ScratchpadWriteGoogle.js");
+
+__importedLesson = [	
+	// review	
+	GoogleDiv,
+	GoogleDivWithinDiv,
+	SearchDiv,
+	FeelingLuckyDiv,
+	StyleGoogleHor,
+	StyleGoogleVert,
+	StyleGoogleFont,
+	MoveSearch,
+	MoveLucky,
+	AddImage,
+	ButtonBorders
+	// new stuff
+
+]
