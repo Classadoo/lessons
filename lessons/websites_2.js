24af1cec791c378a5763a0ace3500cf2a0d8e88a
David Gaynor <david.gaynor@students.olin.edu>
2016-04-14 14:47:25 -0700

    added videos

--- lessons/websites_2.js
+++ lessons/websites_2.js
@@ -1,18 +1,21 @@
 require("../tasks/ScratchpadWriteGoogle.js");
+require("../tasks/ChangeTheNews.js");
 
 __importedLesson = [	
 	// review	
-	GoogleDiv,
-	GoogleDivWithinDiv,
-	SearchDiv,
-	FeelingLuckyDiv,
-	StyleGoogleHor,
-	StyleGoogleVert,
-	StyleGoogleFont,
-	MoveSearch,
-	MoveLucky,
-	AddImage,
-	ButtonBorders
-	// new stuff
+	// GoogleDiv,
+	// GoogleDivWithinDiv,
+	// SearchDiv,
+	// FeelingLuckyDiv,
+	// StyleGoogleHor,
+	// StyleGoogleVert,
+	// StyleGoogleFont,
+	// MoveSearch,
+	// MoveLucky,
+	// AddImage,
+	// ButtonBorders,
 
+	// new stuff
+	ChangePageText,
+	ChangeImgSrc,
 ]
