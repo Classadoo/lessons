f00f6f8929876e2463e0344e96d8c3f3b620ad70
David Gaynor <david.gaynor@students.olin.edu>
2016-05-12 15:43:38 -0700

    building for the deploy

---
+++ lessons/fh_5.js
@@ -0,0 +1,38 @@
+require("../tasks/PuzzleSite.js");
+require("../tasks/ChangeTheNews.js");
+require("../tasks/MakeTheNews.js");
+require("../tasks/CssPractice.js");
+
+__importedLesson = [	
+	//puzzle
+	Challenge1,
+	
+	// make news site	
+	AddNewsTitle,
+	AddNewsTitleId,
+	AddNewsStyleTag,	
+	CenterNewsTitle,
+	MakeTitleBold,
+	BigNewsTitle,
+	CreateNewsImage,	
+	CenterNewsImage,
+
+	// break and talk about inline vs. block
+
+	InvestigateInlineNewsImage,
+	PutNewsImageInsideDiv,	
+	ChangeImageWidth,
+	ChangeNewsImageMarginTop,
+	AddArticleDiv,		
+	AddArticles,
+
+	// break and walk through this part together
+
+	GiveArticlesIds,
+	MarkArticlesInline,
+	AddArticleText,
+	MakeArticlesInlineBlock,
+	GiveArticlesFixedWidth,
+	CenterArticles,
+	AdjustArticlesMarginTop
+]
